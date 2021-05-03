a942f1fe166b4ab54e202b4a61911110b0ee499b
sgtpeter30 <68299466+sgtpeter30@users.noreply.github.com>
2021-05-03 20:32:44 +0200

    react-create-app on Heroku

--- src/molecules/Header.jsx
+++ src/molecules/Header.jsx
@@ -1,7 +1,7 @@
 // import React, { useContext } from "react";
 import { useLocation } from 'react-router-dom';
-
-// import "./Header.scss";
+import logo from "../icons/logo.svg";
+import "../styles/Header.scss";
 
 const Header = () => {
   let location = useLocation();
@@ -10,13 +10,14 @@ const Header = () => {
   location.pathname === '/' &&  document.querySelector('header') !== null ? document.querySelector('header').classList.add('d-none') : console.log("a tutaj już nie")
 
   return (
-    <header className="navbar navbar-dark navbar-expand-md bg-primary d-flex justify-content-between flex-wrap align-items-center">
-      <span>To jest nasz header</span>
-      <ul className="navbar-nav justify-content-start">
-        <li className="nav-item"></li>
-        <li className="nav-item"></li>
+    <header className="navbar navbar-dark navbar-expand-md bg-white d-flex justify-content-between flex-wrap align-items-center mt-5">
+      <ul className="navbar-nav justify-content-between align-items-center w-100 container">
+        <li className="nav-item col-2"><a href="/equipment" className="btn p-5 text-end">Wypożyczalnie sprzętu</a></li>
+        <li className="nav-item col-2"><a href="/houses" className="btn p-5 text-end">Usługi budowlane</a></li>
+        <li className="nav-item col-2 logo"><a href="/bud-walczak"><img src={logo} alt="bud-walczak_logo" /></a></li>
+        <li className="nav-item col-2"><a href="/about-us" className="btn p-5">O firmie</a></li>
+        <li className="nav-item col-2"><a href="/contact" className="btn p-5">Kontakt</a></li>
       </ul>
-      <button className="btn btn-secondary">Nowy button</button>
     </header>
   );
 };
